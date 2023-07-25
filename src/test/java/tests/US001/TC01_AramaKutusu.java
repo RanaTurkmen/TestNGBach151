590c8d7d7e5a95d11da29094e132b2c69487af3a
MehmetFatih57 <130609519+MehmetFatih57@users.noreply.github.com>
2023-07-25 03:54:29 +0300

    Update TC01_AramaKutusu.java

--- src/test/java/tests/US001/TC01_AramaKutusu.java
+++ src/test/java/tests/US001/TC01_AramaKutusu.java
@@ -6,13 +6,6 @@ import pages.Page;
 import utilities.ConfigReader;
 import utilities.Driver;
 
-public class TC01_AramaKutusu {
-    @Test
-    public void test01() {
-        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
-        Page page = new Page();
-        page.aramaKutusu.sendKeys("Iphone" , Keys.ENTER);
-        Driver.closeDriver();
-        //Amazon sayfasina gidilip arama kutusu test edildi
-    }
+public class TC01_KayitIslemi {
+   
 }
