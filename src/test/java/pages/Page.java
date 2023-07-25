948ed5f2fcd80310799f20322db1a759636e135e
Rana Turkmen <ranabetulturkmen@gmail.com>
2023-07-25 19:08:07 +0300

    US009-TC001

--- src/test/java/pages/Page.java
+++ src/test/java/pages/Page.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -206,59 +208,58 @@ public class Page {
 
 
 
-
     //Furkan-US-07-08
-    //Rana-US-09-10
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //Rana-US-09-10/211-260
+    @FindBy(xpath = "//a[@class='register inline-type']")
+    public WebElement registion;
+    @FindBy(xpath = "(//a[@href='https://allovercommerce.com/vendor-register/'])[2]")
+    public WebElement BecomeVendor;
+    @FindBy(xpath = "(//h2)[1]")
+    public WebElement VendorRegistrationYazisi;
+    @FindBy(xpath = "(//a[@title='Copy this email address'])[2]")
+    public WebElement fakeEmailCopy;
+    @FindBy(xpath = "//tr[@class='hidden-xs hidden-sm klikaciRadek newMail']")
+    public WebElement fakeEmailCode;
+    @FindBy(xpath = "(//span[@class='wcicon-status-cancelled'])[1]")
+    public WebElement This_field_is_required;//4 mesajı tab yaparak dogrula
+    @FindBy(xpath = "(//input)[3]")
+    public WebElement email;
+    @FindBy(xpath = "//div[@class='wcfm-message email_verification_message wcfm-success']")
+    public WebElement verification;
+    @FindBy(xpath = "//input[@name='wcfm_email_verified_input']")
+    public WebElement veryficationCodeKutusu;
+    @FindBy(xpath = "//strong[text()='Email']")
+    public WebElement bosluk;
+    @FindBy(xpath = "(//input)[5]")
+    public WebElement resendCode;
+    @FindBy(css = "#passoword")
+    public WebElement password;
+    @FindBy(xpath = "(//input)[7]")
+    public WebElement confirmPassword;
+    @FindBy(xpath = "(//input)[8]")
+    public WebElement submit;
+    @FindBy(id = "password_strength")
+    public WebElement verifyPassword;
+    @FindBy(xpath = "//*[@class='wcfm-message email_verification_message wcfm-success']")
+    public WebElement successemail;
+    @FindBy(xpath = "//div[@class='wcfm-message email_verification_message wcfm-error']")
+    public WebElement erroremail;
+    @FindBy(xpath = "//div[@class='wcfm-message wcfm-success']")
+    public WebElement successlogin;
+    @FindBy(xpath = "//div[@class='wcfm-message wcfm-error']")
+    public WebElement messageError;
+    @FindBy(xpath = "(//h1)[2]")
+    public WebElement welcome;
+    @FindBy(xpath = "//b")
+    public WebElement code;
 
     //Rana-US-09-10
     //Ali-US-11-12
+
+
+
+
+
 
 
 
